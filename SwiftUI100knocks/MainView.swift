7ed9746051aefac861bdd579238f901acc31eadf
okada-yuka <yuka_okada_1022-dev@yahoo.co.jp>
2024-02-23 13:18:40 +0900

    000_MainViewを追加

--- SwiftUI100knocks/MainView.swift
+++ SwiftUI100knocks/MainView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  MainView.swift
 //  SwiftUI100knocks
 //
 //  Created by Yuka Okada on 2024/02/23.
@@ -7,18 +7,132 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct MainView: View {
+    init() {
+        UIPageControl.appearance().currentPageIndicatorTintColor = .systemGray
+        UIPageControl.appearance().pageIndicatorTintColor = .systemGray4
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView {
+            List {
+                Section(header:
+                    Text("1から10本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                        Text("Page01")
+                        Text("Page02")
+                    }
+                    .frame(height: 300)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("11から20本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("21から30本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("31から40本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("41から50本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("51から60本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("61から70本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("71から80本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("81から90本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+                
+                Section(header:
+                    Text("91から100本")
+                    .font(.title2)
+                ) {
+                    TabView {
+                    }
+                    .frame(height: 300)
+                    .frame(maxWidth: .infinity)
+                    .tabViewStyle(.page(indexDisplayMode: .automatic))
+                }
+            }
+            .listStyle(.insetGrouped)
+            .navigationTitle("SwiftUI 100本ノック")
         }
-        .padding()
     }
 }
 
 #Preview {
-    ContentView()
+    MainView()
 }
