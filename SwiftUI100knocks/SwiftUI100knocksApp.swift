1cfa04a2d572cfb6f2eed75ab0e28e77dfb3b8e0
okada-yuka <yuka_okada_1022-dev@yahoo.co.jp>
2024-02-23 12:18:26 +0900

    Initial Commit

---
+++ SwiftUI100knocks/SwiftUI100knocksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI100knocksApp.swift
+//  SwiftUI100knocks
+//
+//  Created by Yuka Okada on 2024/02/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI100knocksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
